8f84773e8e580b099a901164724b48a967ab55f7
Vishwa Vivek Yadav <112635764+Vishwa9011@users.noreply.github.com>
2023-03-02 18:48:04 +0530

    initial setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ChakraProvider from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
 )
